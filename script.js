32df2db10873dbdced8f89b43e33ee5a24c7b2ba
Gajendra Mahato <62411697+NoobGajen@users.noreply.github.com>
2024-01-31 16:42:05 +0545

    Update script.js

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function () {
 
   // typing text animation script
   var typed = new Typed('.typing', {
-    strings: ['Penetration Tester', 'Ethical Hacker', 'Freelancer'],
+    strings: ['Pentester', 'Ethical Hacker', 'Freelancer'],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true,
   });
 
   var typed = new Typed('.typing-2', {
-    strings: ['Penetration Tester', 'Ethical Hacker', 'Freelancer'],
+    strings: ['Pentester', 'Ethical Hacker', 'Freelancer'],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true,
